51e82be21686f6d2715f156310d32ea187b3d665
Bildad Ochieng Masaga <bijasoto@gmail.com>
2026-01-21 18:41:59 +0300

    added a function to fetch jokes after mount

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ import FetchButton from './components/FetchButton'
 function App() {
   // Step 1: Create state variables for `joke` and `loading`
   const [joke, setJoke] = useState("");
-  const [loading, setLoading] = useState("Loading");
+  const [loading, setLoading] = useState(true);
   // Step 2: Use `useEffect` to call a function that fetches a joke when the component mounts
   useEffect(() => {
     async function fetchJoke() {
       const response = await fetch("https://v2.jokeapi.dev/joke/Programming?type=single")
-      const data = await response.json;
+      const data = await response.json();
       setJoke(data.joke);
     }
     fetchJoke();
@@ -21,9 +21,19 @@ function App() {
   // - Update the `joke` state with the fetched joke
   // - Set `loading` to false once the joke is loaded
   // - Handle any errors in the `.catch` block
-function FetchNewJoke (){
- setLoading(true); 
-}
+  async function fetchNewJokes() {
+    setLoading(true);
+    try {
+      const response = await fetch("https://v2.jokeapi.dev/joke/Programming?type=single")
+      const data = await response.json();
+      setJoke(data.joke);
+    } catch (error) {
+      console.error("Error fetching joke:", error)
+    } finally {
+      setLoading(false)
+    }
+  };
+
   return (
     <div className="app">
       <h1>Programming Jokes</h1>
